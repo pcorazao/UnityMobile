28209c3d0309ca45ab7452be5a0e08ffb547dd65
Peter Corazao <pcorazao@nc2media.com>
2015-03-26 18:54:14 -0500

    moves the cube

---
+++ UnityMobile/Assets/scripts/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+			if (Input.touchCount == 1) {
+			transform.Translate(Input.touches[0].deltaPosition.x * .25f,
+			                    Input.touches[0].deltaPosition.y * .25f,
+			                    0);
+			}
+	}
+}
