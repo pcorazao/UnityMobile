c1ecbae3d7b8d1f73b81ab8b0c5fd0c364796914
Peter Corazao <pcorazao@nc2media.com>
2015-03-26 19:00:30 -0500

    cube drags around.

--- UnityMobile/Assets/scripts/MoveScript.cs
+++ UnityMobile/Assets/scripts/MoveScript.cs
@@ -12,9 +12,16 @@ public class MoveScript : MonoBehaviour {
 	void Update () {
 	
 			if (Input.touchCount == 1) {
-			transform.Translate(Input.touches[0].deltaPosition.x * .25f,
-			                    Input.touches[0].deltaPosition.y * .25f,
-			                    0);
+//			transform.Translate(Input.touches[0].deltaPosition.x * .25f,
+//			                    Input.touches[0].deltaPosition.y * .25f,
+//			                    0);
+
+				Touch touch = Input.GetTouch(0);
+				float x = -7.5f + 15 * touch.position.x / Screen.width;
+				float y = -4.5f + 9 * touch.position.y / Screen.height;
+
+			transform.position = new Vector3(x,y,0);
+
 			}
 	}
 }
